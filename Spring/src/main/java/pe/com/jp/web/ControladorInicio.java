e6dab0a43e96e81faa3b1dc106c4576dd2d7c809
JPiert <jpalominoq@unsa.edu.pe>
2022-02-25 17:20:29 -0500

    CRUD de la clase persona

--- Spring/src/main/java/pe/com/jp/web/ControladorInicio.java
+++ Spring/src/main/java/pe/com/jp/web/ControladorInicio.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import pe.com.jp.Service.PersonaService;
+import pe.com.jp.domain.Persona;
 
 
 @Controller
@@ -19,5 +21,25 @@ public class ControladorInicio {
         log.info("Ejecutando el controlador SPRING MVC");
         model.addAttribute("personas", personas);
         return "index";
+    }
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+    @GetMapping("/editar/{id_persona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarpersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
     }
 }
