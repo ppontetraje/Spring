1e9a33e0546dc00ef6bfd2a627d40f724ec4152f
JPiert <jpalominoq@unsa.edu.pe>
2022-02-28 17:57:21 -0500

    Se añadió configuración de roles, para los usuarios, además una plantilla 403 de acceso denegado. se configuraron todos los paths, además se implementó una vista de login.

--- Spring/src/main/java/pe/com/jp/web/SecurityConfig.java
+++ Spring/src/main/java/pe/com/jp/web/SecurityConfig.java
@@ -2,6 +2,7 @@ package pe.com.jp.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -18,5 +19,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user")
                     .password("{noop}123")
                     .roles("USER");
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/editar/**", "/agregar/**","/eliminar")
+                    .hasRole("ADMIN")
+                .antMatchers("/")
+                    .hasAnyRole("USER","ADMIN")
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                .and()
+                    .exceptionHandling().accessDeniedPage("/errors/403")
+                ;
     }
 }
--- Spring/src/main/java/pe/com/jp/web/WebConfig.java
+++ Spring/src/main/java/pe/com/jp/web/WebConfig.java
@@ -1,10 +1,12 @@
 package pe.com.jp.web;
 
+import java.util.HashSet;
 import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -25,8 +27,16 @@ public class WebConfig implements WebMvcConfigurer {
         return lci;
     }
     @Override
-    public void addInterceptors(InterceptorRegistry register){
-        register.addInterceptor(localeChangeInterceptor());
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login");
+        registry.addViewController("/errors/403").setViewName("/errors/403");
+        ;
+        
     }
             
 }
